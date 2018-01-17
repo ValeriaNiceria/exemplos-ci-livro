732c29a50f12455cf34e7842cc2c5af193dfc352
ValeriaNiceria <valerianiceria@gmail.com>
2018-01-16 23:34:40 -0200

    Passando dados do controller para view

--- site-institucional/application/controllers/Institucional.php
+++ site-institucional/application/controllers/Institucional.php
@@ -5,18 +5,33 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Institucional extends CI_Controller {
     public function index()
     {
-        $this->load->view('home');
+        $data['title'] = "LCI | Home";
+        $data['description'] = "Exercício de exemplo do capítulo 5 do livro CodeIgniter";
+
+        $this->load->view('home', $data);
     }
 
     public function Empresa()
     {
         $this->load->view('commons/header');
-        $this->load->view('empresa');
+
+        $data['title'] = "LCI | A Empresa";
+        $data['description'] = "Informações sobre a empresa";
+
+        $this->load->view('empresa', $data);
+
         $this->load->view('commons/footer');
     }
 
     public function Servicos()
     {
+        $this->load->view('commons/header');
+
+        $data['title'] = "LCI | Serviços";
+        $data['description'] = "Informações sobre os serviços prestados";
+
         $this->load->view('servicos');
+
+        $this->load->view('commons/footer');
     }
 }
