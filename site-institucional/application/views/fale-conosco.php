489d818814f9b4a0eca3de8230138e33e414443b
ValeriaNiceria <valerianiceria@gmail.com>
2018-01-18 17:22:04 -0200

    Validando o formulário de contato

--- site-institucional/application/views/fale-conosco.php
+++ site-institucional/application/views/fale-conosco.php
@@ -6,19 +6,31 @@
     </div>
     <div class="row">
         <div class="col-md-8">
-            <form action="" method="POST" class="form-horizontal">
+
+        <!--Exibir a mensagem-->
+            <?php if ($formErrors) : ?>
+                <div class="alert alert-danger">
+                    <?=$formErrors?>
+                </div>
+            <?php elseif ($this->session->flashdata('success_msg')) : ?>
+                <div class="alert alert-success">
+                    <?=$this->session->flashdata('sucess_msg')?>
+                </div>
+            <?php endif; ?>
+
+            <form action="<?=base_url('fale-conosco')?>" method="POST" class="form-horizontal">
                 
                 <div class="form-group">
                     <label for="nome" class="col-md-2 control-label">Nome</label>
                     <div class="col-md-8">
-                        <input id="nome" name="nome" type="text" placeholder="Nome" required="" class="form-control input-md">
+                        <input id="nome" name="nome" type="text" placeholder="Nome" required="" class="form-control input-md" value="<?=set_value('nome')?>">
                     </div>
                 </div>
 
                 <div class="form-group">
                     <label for="email" class="col-md-2 control-label">Email</label>
                     <div class="col-md-8">
-                        <input id="email" name="email" placeholder="Email"  required="" type="text" class="form-control input-md">
+                        <input id="email" name="email" placeholder="Email"  required="" type="text" class="form-control input-md" value="<?=set_value('email')?>">
                         <span class="help-block">Ex.: email@exemple.com</span>
                     </div>
                 </div>
@@ -26,14 +38,14 @@
                 <div class="form-group">
                     <label for="assunto" class="col-md-2 control-label">Assunto</label>
                     <div class="col-md-8">
-                        <input type="text" id="assunto" name="assunto" placeholder="Assunto" required="" class="form-control input-md">
+                        <input type="text" id="assunto" name="assunto" placeholder="Assunto" required="" class="form-control input-md" value="<?=set_value('assunto')?>">
                     </div>
                 </div>
 
                 <div class="form-group">
                     <label for="mensagem" class="col-md-2 control-label">Mensagem</label>
                     <div class="col-md-8">
-                        <textarea name="mensagem" id="mensagem" rows="10" placeholder="Mensagem" class="form-control"></textarea>
+                        <textarea name="mensagem" id="mensagem" rows="10" placeholder="Mensagem" class="form-control"><?=set_value('mensagem')?></textarea>
                     </div>
                 </div>
 
