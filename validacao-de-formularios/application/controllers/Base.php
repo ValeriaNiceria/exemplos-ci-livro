4e46b554ae19849c5b131dfd6d6677c5505e7dd1
ValeriaNiceria <valerianiceria@gmail.com>
2018-01-22 19:56:13 -0200

    Formulário

--- validacao-de-formularios/application/controllers/Base.php
+++ validacao-de-formularios/application/controllers/Base.php
@@ -6,7 +6,9 @@ Class Base extends CI_Controller {
 
     public function index() 
     {
-        $this->load->view('home');
+        $data['title'] = "Estudo | Validação de formulários";
+        $data['description'] = "Capítulo 7 - Validando formulários";
+        $this->load->view('home', $data);
     }
 
 }
---
+++ validacao-de-formularios/application/views/commons/footer.php
@@ -0,0 +1,13 @@
+    <footer class="footer">
+        <div class="container">
+            <p class="text-muted">&copy; Copyright 2018</p>
+        </div>
+    </footer>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack para Surface/desktop Windows 8 -->
+    <script src="http://getbootstrap.com/assets/assets/js/ie10-viewport-bug-workaround.js"></script>
+</body>
+</html>
