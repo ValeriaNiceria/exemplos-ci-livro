9af3a941015976c27c1b7afda407290365d853a4
ValeriaNiceria <valerianiceria@gmail.com>
2018-01-23 17:53:37 -0200

    Configurações Iniciais do CodeIgniter

---
+++ curso-codeigniter/application/views/site.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Bem vindo ao Curso CodeIgniter</title>
+</head>
+<body>
+
+<div id="container">
+	<h1>Bem vindo ao CodeIgniter!</h1>
+
+	<div id="body">
+		Página inicial do curso de codeigniter
+	</div>
+
+</div>
+
+</body>
+</html>
