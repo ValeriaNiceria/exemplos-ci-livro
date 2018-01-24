fd32c45c8c2427b2f4b2ce8d3621ef7289aec8dd
ValeriaNiceria <valerianiceria@gmail.com>
2018-01-24 15:26:01 -0200

    Select nos Models

--- curso-codeigniter/application/models/Filmes.php
+++ curso-codeigniter/application/models/Filmes.php
@@ -8,8 +8,13 @@ class Filmes extends CI_Model {
 
     public function listar_filmes() {
         //$sql = "SELECT * FROM filmes";
-        $this->db->select();
+        //$this->db->select('filme_nome', 'filme_preco');
+        //$this->db->select()->from("filmes");
+        //$this->db->select()->from('filmes')->where('id', 1);   
+        //$this->db->select()->from("filmes")->limit(2);
+        //$this->db->select()->from("filmes")->join('lancamentos', 'filmes.id = lancamentos.filmes');             
         $query = $this->db->get("filmes");
+        //return $query->row(); //Retorna somente um valor
         return $query->result();
     }
 
