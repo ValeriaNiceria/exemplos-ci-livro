793173c4a4cb0a7802b6ea0fdd2b90a2b97f713a
ValeriaNiceria <valerianiceria@gmail.com>
2018-02-04 22:15:00 -0200

    Atualizado o projeto

--- mercado/application/models/Vendas_model.php
+++ mercado/application/models/Vendas_model.php
@@ -12,4 +12,21 @@ class Vendas_model extends CI_Model {
             "id" => $venda["produto_id"]
         ));
     }
+
+    public function busca_vendidos($usuario) 
+    {
+        $id = $usuario["id"];
+
+        $this->db->select("produtos.*, vendas.data_de_entrega");
+
+        $this->db->from("produtos");
+        $this->db->join("vendas", "vendas.produto_id = produtos.id");
+
+        $this->db->where(array(
+            "vendido" => true,
+            "usuario_id" => $id
+        ));
+
+        return $this->db->get()->result();
+    }
 }
